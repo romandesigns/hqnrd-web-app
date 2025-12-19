413ed7ff2bbf0791c60638c42a21be36c965d5c3
Roman F. <wavystyledev@gmail.com>
2025-12-19 19:52:51 +0000

    implemented theme provider

---
+++ providers/index.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "./ThemeProvider";
+
+function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+    );
+}
+
+export default Providers;
